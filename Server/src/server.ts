77f5eb463f246384e8ad42869358e2795b923c46
Andre <andre.almeida3@gmail.com>
2022-11-06 00:48:01 -0300

    .

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     fastify.get('/pools/count', async () => {
 
         const count = await prisma.pool.count()
-        return ({count})
+        return {count}
     })
 
     await fastify.listen({ port: 3333 /*, host: '0.0.0.0'*/ })
